4ab30a25ef60391fe379f4e019ea646d1d66547b
Gink83 <burlakak222@gmail.com>
2023-04-10 22:49:10 +0300

    once and for all

---
+++ uppercase-converter.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+
+std::string text;
+
+std::string upperText(std::string text);
+
+void askForText()
+{
+	system("cls");
+	std::cout << "Enter the text to be converted to uppercase:" << std::endl;
+
+	std::getline(std::cin, text);
+
+	std::cout << std::endl;
+	std::cout << upperText(text);
+	std::cout << std::endl << std::endl;
+
+	system("pause");
+	askForText();
+}
+
+std::string upperText(std::string text)
+{
+	std::string result;
+	for (char i : text) 
+	{
+		result += toupper(i);
+	}
+	return result;
+}
+
+int main()
+{
+	askForText();
+}
